fb0c197f859ba6b1b04b223edc9d5062389ab591
lucas-Engloba <lucas.englobabasistemas@gmail.com>
2022-01-24 10:21:37 -0300

    update script tabela atendimento

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
+const tabelas = require('./infraestrutura/tabelas')
 
 conexao.connect(  erro => {
 
@@ -8,6 +9,8 @@ conexao.connect(  erro => {
     }
     else{
         console.log('Conectado com sucesso')
+
+        tabelas.CriandoTabelasBancoMySql(conexao)// Função para criar as tabelas
 
         const app = customExpress()
         app.listen(3000, ()=> console.log('servidor rodando na porta 3000'))
---
+++ infraestrutura/tabelas.js
@@ -0,0 +1,28 @@
+class Tabelas {
+  CriandoTabelasBancoMySql(conexao) {
+    this.conexao = conexao;
+    this.criarAtendimentos();
+  }
+
+  criarAtendimentos() {
+    const sql = `CREATE TABLE Atendimentos (
+                    id int NOT NULL AUTO_INCREMENT,
+                    cliente varchar(50) NOT NULL,
+                    pet varchar(20),
+                    servico varchar(20) NOT NULL,
+                    status varchar(20) NOT NULL,
+                    observacoes text,
+                    PRIMARY KEY(id)         
+                                            )`;
+
+    this.conexao.query(sql, (erro) => {
+      if (erro) {
+        console.log("Erro ao criar tabela Atendimentos, " + erro);
+      } else {
+        console.log("Tabela Atendimentos criada com sucesso");
+      }
+    });
+  }
+}
+
+module.exports = new Tabelas();
